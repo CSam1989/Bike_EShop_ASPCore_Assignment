a4b6a1bee199515f9166b3778f7e226a56931a36
sam <sam@SAM2016>
2020-09-08 09:40:24 +0200

    Changed images from JPG to PNG

--- Bike.EShop.TagHelpers/BikeImage/BikeImageTagHelper.cs
+++ Bike.EShop.TagHelpers/BikeImage/BikeImageTagHelper.cs
@@ -20,7 +20,7 @@ namespace Bike.EShop.TagHelpers.BikeImage
 
             output.TagName = "img";
             output.Content.SetHtmlContent(
-                $"<img src=\"../images/bikes/bike{RandomNr}.jpg\" alt=\"Image of a bike\"/>"
+                $"<img src=\"../images/bikes/bike{RandomNr}.png\" alt=\"Image of a bike\"/>"
             );
         }
     }
