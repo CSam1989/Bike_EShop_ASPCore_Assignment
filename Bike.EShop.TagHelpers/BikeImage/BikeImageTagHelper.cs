4eec8b3ee6926a6732cc5f1176df1ef9508a4e4f
sam <sam@SAM2016>
2020-09-08 08:30:41 +0200

    Created Tag Helper for random bike image landingpage

---
+++ Bike.EShop.TagHelpers/BikeImage/BikeImageTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Bike.EShop.TagHelpers.NameConsts;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bike.EShop.TagHelpers.BikeImage
+{
+    [HtmlTargetElement(TagHelperNames.BikeImgTagHelper)]
+    public class BikeImageTagHelper: TagHelper
+    {
+        public int RandomNr { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            output.TagName = "img";
+            output.Content.SetHtmlContent(
+                $"<img src=\"../images/bikes/bike{RandomNr}.jpg\" alt=\"Image of a bike\"/>"
+            );
+        }
+    }
+}
---
+++ Bike.EShop.TagHelpers/NameConsts/TagHelperNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bike.EShop.TagHelpers.NameConsts
+{
+    public static class TagHelperNames
+    {
+        /// <summary>
+        ///     The attribute name of the Bike Image.
+        /// </summary>
+        public const string BikeImgTagHelper = "bike-img";
+    }
+}
