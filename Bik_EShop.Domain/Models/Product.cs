95ac5c2e11af4bfd64d2f1fb5e17e22b682990c8
sam <sam@SAM2016>
2020-09-08 10:21:12 +0200

    Linked Landing page with Products page

---
+++ Bik_EShop.Domain/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bik_EShop.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
