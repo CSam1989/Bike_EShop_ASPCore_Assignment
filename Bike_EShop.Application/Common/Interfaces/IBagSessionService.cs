f40be46eb5c6037a7de95a15441063be506fcdf3
sam <sam@SAM2016>
2020-09-10 16:29:43 +0200

    Storing bag id in session, created services to handle this

---
+++ Bike_EShop.Application/Common/Interfaces/IBagSessionService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Bike_EShop.Application.Common.Interfaces
+{
+    public interface IBagSessionService
+    {
+        Task<int> RetrieveBagIdFromSession();
+    }
+}
---
+++ Bike_EShop.Web/Common/Services/BagSessionService.cs
@@ -0,0 +1,39 @@
+﻿using Bike_EShop.Application.Common.Interfaces;
+using Bike_EShop.Application.Shoppingbags.Commands.Create;
+using Bike_EShop.Web.Common.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bike_EShop.Web.Common.Services
+{
+    public class BagSessionService : IBagSessionService
+    {
+        private readonly IMediator _mediator;
+        private readonly IHttpContextAccessor _accessor;
+
+        public BagSessionService(IMediator mediator, IHttpContextAccessor accessor)
+        {
+            this._mediator = mediator;
+            this._accessor = accessor;
+        }
+
+        public async Task<int> RetrieveBagIdFromSession()
+        {
+            var bagId = _accessor.HttpContext.Session.GetObjectFromJson<string>("bagId");
+
+            if (bagId is null)
+            {
+                //CustomerId wordt nog aangepast van zodra Identity Authentication is ingevoerd
+                var id = await _mediator.Send(new CreateShoppingBagCommand { CustomerId = 1});
+                bagId = id.ToString();
+                _accessor.HttpContext.Session.SetObjectAsJson("bagId", bagId);
+            }
+
+            return int.Parse(bagId);
+        }
+    }
+}
