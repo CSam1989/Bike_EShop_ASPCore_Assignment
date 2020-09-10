bfb4b3e0ad8e6a4616449626d1afc17a88dc9420
sam <sam@SAM2016>
2020-09-10 08:52:35 +0200

    delete product backend

---
+++ Bike_EShop.Application/Products/Commands/Delete/DeleteProductCommand.cs
@@ -0,0 +1,40 @@
+﻿using Bike_EShop.Application.Common.Exceptions;
+using Bike_EShop.Application.Common.Interfaces;
+using Bike_EShop.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bike_EShop.Application.Products.Commands.Delete
+{
+    public class DeleteProductCommand:IRequest
+    {
+        public int Id { get; set; }
+
+        public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public DeleteProductCommandHandler(IApplicationDbContext context)
+            {
+                this._context = context;
+            }
+            public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+            {
+                var entity = await _context.Products.FindAsync(request.Id, cancellationToken);
+
+                if (entity is null)
+                    throw new NotFoundException(nameof(Product), request.Id);
+
+                _context.Products.Remove(entity);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
