142a60a03c37b6234a2ac4de01d43b43691bd6f0
sam <sam@SAM2016>
2020-09-10 10:14:26 +0200

    get product by id backend (for detail page & edit page)

---
+++ Bike_EShop.Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Bike_EShop.Application.Common.Exceptions;
+using Bike_EShop.Application.Common.Interfaces;
+using Bike_EShop.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bike_EShop.Application.Products.Queries.GetProductById
+{
+    public class GetProductByIdQuery: IRequest<ProductByIdVM>
+    {
+        public int Id { get; set; }
+
+        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductByIdVM>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+
+            public GetProductByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+            {
+                this._context = context;
+                this._mapper = mapper;
+            }
+
+            public async Task<ProductByIdVM> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+            {
+                var product = await _context.Products.FindAsync(request.Id, cancellationToken);
+
+                if (product is null)
+                    throw new NotFoundException(nameof(Product), request.Id);
+
+                return new ProductByIdVM
+                {
+                    Product = _mapper.Map<ProductByIdDto>(product)
+                };
+            }
+        }
+    }
+}
