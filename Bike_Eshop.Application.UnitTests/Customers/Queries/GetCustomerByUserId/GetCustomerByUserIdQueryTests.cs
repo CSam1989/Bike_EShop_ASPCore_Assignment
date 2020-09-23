778244b16a1c3982ca11d36a16c6392979b1aac6
sam <sam@SAM2016>
2020-09-23 14:18:48 +0200

    get customer by user id query tests

---
+++ Bike_Eshop.Application.UnitTests/Customers/Queries/GetCustomerByUserId/GetCustomerByUserIdQueryTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Bike_EShop.Application.Common.Exceptions;
+using Bike_EShop.Application.Customers.Queries.GetCustomerByUserId;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Bike_Eshop.Application.UnitTests.Customers.Queries.GetCustomerByUserId
+{
+    public class GetCustomerByUserIdQueryTests
+    {
+        private GetCustomerByUserIdQuery.GetCustomerByUserIdQueryHandler _handler;
+        private const string ExistingUserId = "000-000-000";
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fixture = new QueryTestFixture();
+            _handler = new GetCustomerByUserIdQuery.GetCustomerByUserIdQueryHandler(fixture.Context, fixture.Mapper);
+        }
+
+        [Test]
+        public async Task Handle_GivenValidId_ReturnsCorrectVmAndObject()
+        {
+            var query = new GetCustomerByUserIdQuery { UserId =  ExistingUserId};
+
+            var result = await _handler.Handle(query, CancellationToken.None);
+            var customer = result.Customer;
+
+            result.ShouldBeOfType<CustomerByUserIdVM>();
+            customer.Id.ShouldBe(1);
+        }
+
+        [Test]
+        public void Handle_GivenInvalidId_ThrowsException()
+        {
+            var query = new GetCustomerByUserIdQuery { UserId = "not existing userId" };
+
+            Should.ThrowAsync<NotFoundException>(() =>
+                _handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
